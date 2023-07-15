b2e74e495b7cecbd2d61643fb0a20e3b14978151
bilal-bin-yar <bilalbinyar@neurog.ai>
2023-07-15 21:38:26 +0500

    Navbar Added

---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="nav-wrapper">
+          <div className="nav-logo">
+            <h2>Alaa Elden</h2>
+          </div>
+          <div className="nav-links">
+            <ul className="nav-ul">
+              <li>Home</li>
+              <li>Contact</li>
+              <li>About</li>
+              <li>Sign Up</li>
+            </ul>
+          </div>
+          <div className="search-cart">
+            <div className="nav-search-bar">
+              <input type="text" placeholder="What are you looking for?" />
+            </div>
+            <div className="cart-icon-div">
+              <AiOutlineShoppingCart className="cart-icon" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
